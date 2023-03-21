dcc8b85e5809a7632d7ca5f63df6bec213328475
Charles MANGENEY <contact@glm-system.fr>
2023-03-21 13:27:58 +0100

    Add TLS SChannel Set command

--- powershell-checkTLS.ps1
+++ powershell-checkTLS.ps1
@@ -65,3 +65,57 @@ Function Show-TLSRegValues {
     }
     return $array
 }
+
+
+function Set-TLSSChannel
+{
+    param(
+        [Parameter(Mandatory)]
+        [ValidateSet("Client", "Server")]
+        $scope = "client",
+        [ValidateSet("1.0", "1.1", "1.2")]
+        $version,
+        $enabled,
+        $DisabledByDefault,
+        $force
+    )
+
+    if($enabled -ne $null){
+        if($enabled -eq $true){$enabled = 1}
+        elseif($enabled -eq $false){$enabled = 0}
+        if($enabled -in (0,1)){
+            $registry = $TLSRegValues | ? {$_.Path -match $scope -and $_.Path -match $version} 
+            $currentValue = Show-TLSRegValues -regCollection $registry | ? Name -eq Enabled
+            if($currentValue.value -in (0,1)){
+                Set-ItemProperty -Path $currentValue.Path -Name Enabled -Value $enabled
+            }else{
+                if(-not(Test-Path $registry.Path)){
+                    New-Item -Path $registry.Path -Force | Out-Null
+                }
+                New-ItemProperty -Path $registry.Path -Name Enabled -Value $enabled | Out-Null
+            }
+        }
+        
+    }
+
+    if($DisabledByDefault  -ne $null){
+        if($DisabledByDefault -eq $true){$DisabledByDefault = 1}
+        elseif($DisabledByDefault -eq $false){$DisabledByDefault = 0}
+        if($DisabledByDefault -in (0,1)){
+            $registry = $TLSRegValues | ? {$_.Path -match $scope -and $_.Path -match $version} 
+            $currentValue = Show-TLSRegValues -regCollection $registry | ? Name -eq DisabledByDefault
+            if($currentValue.value -in (0,1)){
+                Set-ItemProperty -Path $currentValue.Path -Name DisabledByDefault -Value $DisabledByDefault
+            }else{
+                if(-not(Test-Path $registry.Path)){
+                    New-Item -Path $registry.Path -Force | Out-Null
+                }
+                New-ItemProperty -Path $registry.Path -Name DisabledByDefault -Value $DisabledByDefault | Out-Null
+            }
+        }
+    }
+}
+
+Set-TLSSChannel -scope Server -version 1.1 -enabled $false -DisabledByDefault $true
+Set-TLSSChannel -scope Server -version 1.0 -enabled $false -DisabledByDefault $true
+Set-TLSSChannel -scope Server -version 1.2 -enabled $true -DisabledByDefault $false
