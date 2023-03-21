d9a142cd2bc5a621ab89792e493c9f39cc07452f
Charles MANGENEY <contact@glm-system.fr>
2023-03-21 11:11:26 +0100

    Initialisation du repository

---
+++ powershell-checkTLS.ps1
@@ -0,0 +1,67 @@
+﻿$TLSRegValues = @()
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319"
+    "Keys" = @("SchUseStrongCrypto","SystemDefaultTlsVersions")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727"
+    "Keys" = @("SchUseStrongCrypto","SystemDefaultTlsVersions")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+$TLSRegValues += [PSCustomObject]@{
+    "Path" = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client"
+    "Keys" = @("Enabled","DisabledByDefault")
+}
+
+Function Show-TLSRegValues {
+    [CmdletBinding()]
+    Param
+    (
+        $regCollection = $TLSRegValues
+    )
+    $array = @()
+    foreach($registry in $regCollection){
+        if(Test-Path $registry.path){
+            foreach($name in $registry.keys){
+                $value = Get-ItemProperty -Path $registry.path -Name $name -ErrorAction Ignore
+                $output = [PSCustomObject]@{
+                    Path = $registry.path
+                    Name = $name
+                    value = "Not Found"
+                }
+                If ($value -ne $null) {
+                    $output.Value = $value.$name
+                }
+                $array += $output
+            }
+        }
+    }
+    return $array
+}
